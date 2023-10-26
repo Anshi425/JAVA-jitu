140c2b53f2c1dee56857f125fb6a92f7c4b2b74e
capt-price <73271099+diabolical-duck@users.noreply.github.com>
2023-10-26 12:23:19 +0530

    Add files via upload

---
+++ src/com/app/core/Category.java
@@ -0,0 +1,7 @@
+package com.app.core;
+
+public enum Category {
+
+	MENS_TSHIRT, MENS_SHIRTS, WOMENS_JEANS;
+	
+}
---
+++ src/custom_exceptions/FashionHandlingException.java
@@ -0,0 +1,11 @@
+package custom_exceptions;
+
+@SuppressWarnings("serial")
+public class FashionHandlingException extends Exception {
+	
+	public FashionHandlingException(String mesg) {
+	
+		super(mesg);
+	}
+
+}
